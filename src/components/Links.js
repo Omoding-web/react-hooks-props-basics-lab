37f0b6e2c02330f9dc6756f12797006e6a940d4a
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-01-30 17:24:01 +0300

    Assignment done

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+export default function Links(github, linkedIn){
+
+    return (
+      <>
+    <h3>Links</h3>
+    <a href= {github}> {"https://github.com/liza"}</a>
+    <a href= {linkedIn}> {"https://www.linkedin.com/in/liza/"}</a>
+      </>
+    );
+    }
+  
+  
